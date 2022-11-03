e0c20ebdac10f0a2ff14d8b0bbc7add827840912
M1rzohidovich <munavvarjonovayyubxon@gmail.com>
2022-11-03 16:14:41 -0700

    Update settings.py

--- ServiceProject/settings.py
+++ ServiceProject/settings.py
@@ -134,7 +134,7 @@ WSGI_APPLICATION = 'ServiceProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"DATABASE_URL"}
+    "default": "DATABASE_URL"
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'service',
