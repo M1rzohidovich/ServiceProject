5cc6119d0ef37c4e9d8d3557887b4a841c7c736b
M1rzohidovich <munavvarjonovayyubxon@gmail.com>
2022-11-04 00:44:21 -0700

    update Admin panel

--- ServiceProject/settings.py
+++ ServiceProject/settings.py
@@ -232,3 +232,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+"site_header": "My Service",
+"show_ui_builder": True,
+}
