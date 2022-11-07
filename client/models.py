6290c3bf05e0c02873439bbe87039476e35eb7a1
M1rzohidovich <munavvarjonovayyubxon@gmail.com>
2022-11-06 23:33:10 -0800

    Update

--- client/models.py
+++ client/models.py
@@ -7,6 +7,8 @@ class IchkiMijoz(models.Model):
     surname = models.CharField(max_length=60)
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=180)
+    comment = models.TextField()
+    cleaning_date = models.DateTimeField()
     upload_date = models.DateTimeField(auto_now_add=True)
     order = models.ForeignKey(InternalBlog, on_delete=models.CASCADE)
     
@@ -20,6 +22,8 @@ class TashqiMijoz(models.Model):
     surname = models.CharField(max_length=60)
     phone_number = models.CharField(max_length=50)
     address = models.CharField(max_length=180)
+    comment = models.TextField()
+    cleaning_date = models.DateTimeField()
     upload_date = models.DateTimeField(auto_now_add=True)
     order = models.ForeignKey(ExternalBlog, on_delete=models.CASCADE)
 
